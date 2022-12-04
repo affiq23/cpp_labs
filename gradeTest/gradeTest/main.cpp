d97aaf2fc8f5141d730f533edcf34b876fb2abd6
affiq23 <affiqem@gmail.com>
2022-12-04 11:48:16 -0600

    need to fix displayResults

--- gradeTest/gradeTest/main.cpp
+++ gradeTest/gradeTest/main.cpp
@@ -8,7 +8,22 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <array>
 using namespace std;
+
+void displayResults(char arr1[], char arr2[], int numLines){
+    
+    int incorrectAns = 0;
+    double totalGrade = 0;
+    
+    for(int i = 0; i < numLines; i++){
+        if(arr1[i] != arr2[i]){
+            cout << "Question " << i << " has incorrect answer '" << arr2[i] << "', the correct answer is '" << arr1[i] << "'." << endl;
+            incorrectAns++;
+        }
+    }
+    cout << incorrectAns << " were missed out of " << numLines << endl;
+}
 
 
 
@@ -16,28 +31,35 @@ int main() {
     
     string key;
     string responses;
-    char keyArr[50];
-    char resArr[50];
+    int keyAnswers = 0;
+    int resAnswers = 0;
+    int numKeyLines = 0;
+    string keyLine;
+    int numResLines = 0;
+    string resLine;
     
     cout << "Enter the name of the file containing the key." << endl;
     cin >> key;
-    
-    
     fstream keyFile;
     keyFile.open(key, ios::in);
     
+    if(keyFile.is_open()){
+        while(getline(keyFile, keyLine)){
+            numKeyLines++;
+        }
+    }
+    
+    char keyArr[numKeyLines];
     
     //checking if key file is valid and can be opened
     if(keyFile.is_open()){
-        string line;
-        int i = 0;
         char val;
         
         while(!keyFile.eof()){
            
             keyFile.get(val);
-            keyArr[i] = val;
-            i++;
+            keyArr[keyAnswers] = val;
+            keyAnswers++;
         }
         keyFile.close();
     }
@@ -47,22 +69,26 @@ int main() {
     
     cout << "Enter the name of the file containing the responses." << endl;
     cin >> responses;
-    
     fstream responseFile;
     responseFile.open(responses, ios::in);
     
+    if(responseFile.is_open()){
+        while(getline(responseFile, resLine)){
+            numResLines++;
+        }
+    }
+    
+    char resArr[numResLines];
     
     //checking if responses file is valid and can be opened
     if(responseFile.is_open()){
-        string line;
-        int i = 0;
         char val;
         
         while(!responseFile.eof()){
             
             responseFile.get(val);
-            resArr[i] = val;
-            i++;
+            resArr[resAnswers] = val;
+            resAnswers++;
         }
         responseFile.close();
     }
@@ -70,8 +96,13 @@ int main() {
         cout << responses << " could not be opened." << endl;
     }
     
-    //able to read file data and pass into char arrays
-    
+    if(numKeyLines == numResLines){
+        displayResults(keyArr, resArr, numKeyLines);
+    }
+    else{
+        cout << "File error! There is a mismatch between the number of questions and answers." << endl;
+
+    }
     
    
     return 0;
